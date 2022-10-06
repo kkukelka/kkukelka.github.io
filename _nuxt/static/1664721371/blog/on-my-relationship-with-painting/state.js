9c0d977994a7fab7f780d7d8c6cac24f5fe91742
kkukelka <17953978+kkukelka@users.noreply.github.com>
2022-10-06 16:18:19 +0200

    deploy

--- _nuxt/static/1664721371/blog/on-my-relationship-with-painting/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1664721371",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fon-my-relationship-with-painting",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0570ee5c"}}}}(null));
