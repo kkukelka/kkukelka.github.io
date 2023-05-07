7bacd11caa34c1dee72201f8f932d39626a9e739
kkukelka <17953978+kkukelka@users.noreply.github.com>
2023-05-07 07:53:31 +0200

    deploy generated files

---
+++ docs/_nuxt/ProseBlockquote.84c7992b.js
@@ -0,0 +1,1 @@
+import{k as o,o as r,l as t,r as c}from"./entry.c7cda4e2.js";const s={};function l(e,n){return r(),t("blockquote",null,[c(e.$slots,"default")])}const f=o(s,[["render",l]]);export{f as default};
