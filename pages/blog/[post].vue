97851267b55df87011b0f037779bebe59dc8d9ee
kkukelka <17953978+kkukelka@users.noreply.github.com>
2023-12-23 19:07:15 +0100

    feat: add tipjar

--- pages/blog/[post].vue
+++ pages/blog/[post].vue
@@ -51,6 +51,29 @@
           </div>
         </div>
         <ContentRenderer :value="doc" class="nuxt-content" />
+        <section class="flex justify-center mt-8">
+          <NuxtLink
+            to="https://paypal.me/kiliankukelka?country.x=AT&locale.x=de_DE"
+            target="_blank"
+            class="bg-pink-700 text-white font-bold px-4 py-2 rounded-md hover:bg-pink-600"
+          >
+            Tip Jar
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6 group-hover:fill-pink-800 inline transition-colors duration-150 ease-in"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
+              />
+            </svg>
+          </NuxtLink>
+        </section>
         <NuxtLink
           to="/blog"
           class="group hover:text-pink-800 transition-colors duration-150 ease-in"
