62e4d100d01eae83ed38f314e83547d8105e10bf
kkukelka <17953978+kkukelka@users.noreply.github.com>
2024-01-14 18:21:49 +0100

    feat: add nuxt sitemap module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "@nuxtjs/sitemap"],
   app: {
     head: {
       htmlAttrs: {
